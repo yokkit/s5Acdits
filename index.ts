db9875691ba26967bdc186c4cfd10d6504d75ecb
yokkit <yoko.tochigi@gmail.com>
2021-07-07 19:19:38 +0200

    Add exercise1

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const jokeButton = document.querySelector(".acuditButton") as HTMLButtonElement;
+jokeButton.addEventListener("click", ()=>dadJoke());
+
+const dadJoke = async():Promise<void> =>{
+    try{
+        const res = await fetch(
+            'https://icanhazdadjoke.com/',{
+                headers:{
+                    Accept: "application/json"
+                }
+            });
+        const data = await res.json();
+        console.log(data.joke);
+    } catch(error){
+        console.log(`Something went wrong! -- ${error}`);
+    }
+}
+
